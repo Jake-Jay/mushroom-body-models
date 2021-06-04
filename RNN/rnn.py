c03bf7f2383c145613389d7dd0ff784db7cfac5c
Jake Pencharz <jake.jay.p@gmail.com>
2021-06-04 17:14:57 +0200

    Adjust rnn for masking

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -76,9 +76,9 @@ class NeuralRNNModule(pl.LightningModule):
             'monitor': 'val/loss'
         }
 
-
-class NeuralRNN(nn.Module):
-    def __init__(self, input_size=15, hidden_size=15, nonlinearity="tanh"):
+    
+class MaskedNeuralRNN(nn.Module):
+    def __init__(self, input_mask, output_mask, nonlinearity="tanh"):
         super().__init__()
 
         """
@@ -90,12 +90,73 @@ class NeuralRNN(nn.Module):
         See about masking here:
         https://stackoverflow.com/questions/53544901/how-to-mask-weights-in-pytorch-weight-parameters
         """
+        self.input_mask = input_mask
+        self.output_mask = output_mask
+        self.hidden_size = output_mask.shape[0]
+
+        self.W_xh = CustomizedLinear(self.input_mask, bias=True)
+        self.W_hh = CustomizedLinear(self.output_mask, bias=True)
+
+
+        if nonlinearity == "tanh":
+            self.activation = nn.Tanh() 
+        elif nonlinearity == "relu":
+            self.activation = nn.ReLU()
+        else:
+            raise ValueError("Unrecognized activation. Allowed activations: tanh or relu")
+    
+    def forward(self, x):
+        """
+        Inputs:
+        - x: Input tensor (batch_size, seq_len, input_size)
+
+        Outputs:
+        - h_seq: Hidden vector along sequence (batch_size, seq_len, input_size)
+        - h: Final hidden vetor of sequence(1, batch_size, hidden_size)
+        """
+
+        # TODO implement weights matrix that will remove certain recurrent connections
+
+        # Hidden State initialization
+        h = torch.zeros(
+            (x.size(0), self.hidden_size), 
+            device=x.device, 
+            dtype=x.dtype
+        )                                   # (batch, hidden_features)
+
+        h_seq = []
+        for xt in x.unbind(1):
+            # update hidden state
+            h = self.activation(self.W_hh(h) + self.W_xh(xt))
+            h_seq.append(h.expand(1 ,-1, -1))
+
+        # Stack the h_seq list as a tensor along dim 0
+        h_seq = torch.cat(h_seq, 0)         # (seq_len, batch, hidden_features)
+
+        # Batch first
+        h_seq = h_seq.permute(1,0,2)        # (batch, seq_len, hidden_features)
+
+        return h_seq, h
+    
+    
+    
+    
+
+class NeuralRNN(nn.Module):
+    def __init__(self, input_size=15, hidden_size=15, nonlinearity="tanh"):
+        super().__init__()
+
+        """
+        Inputs:
+        - input_size: Number of features in input vector
+        - hidden_size: Dimension of hidden vector
+        - activation: Nonlinearity in cell; 'tanh' or 'relu'
+        """
 
         self.hidden_size = hidden_size
         self.input_size = input_size
 
         self.W_xh = nn.Linear(self.input_size, self.hidden_size, bias=True)
-        # self.M_xh = torch.ones(self.input_size, self.hidden_size)   Mask
         self.W_hh = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
 
         if nonlinearity == "tanh":
@@ -151,6 +212,7 @@ Author's repository:
 https://github.com/uchida-takumi/CustomizedLinear/blob/master/CustomizedLinear.py
 """
 import math
+import pdb
 
 #################################
 # Define custome autograd function for masked connection.
@@ -167,6 +229,11 @@ class CustomizedLinearFunction(torch.autograd.Function):
         if mask is not None:
             # change weight to 0 where mask == 0
             weight = weight * mask
+        
+        
+#         pdb.set_trace()
+#         Somehow input is 3 dimensional torch.Size([1, 8, 15])
+# resulting in it not being a matrix
         output = input.mm(weight.t())
         if bias is not None:
             output += bias.unsqueeze(0).expand_as(output)
